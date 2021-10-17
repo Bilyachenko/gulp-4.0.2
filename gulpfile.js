5a7440d5aff9fb74321e9426241c1b6411fffebc
Max <maxbill20@gmail.com>
2021-10-17 14:12:00 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass')(require('sass')); //npm install --save-dev sass
+var notify = require('gulp-notify');	
+var browserSync = require('browser-sync').create();	
+
+//css congig
+function cssconfig(done) {
+	
+	gulp.src('./project/assets/scss/main.scss')
+		.pipe(sass({
+			errorLogToConsole:true,
+			outputStyle:'compressed'
+		}))
+		.on('error', console.error.bind(console))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(gulp.dest('./project/assets/css/'))
+		.pipe(notify({
+			title: 'Gulp',
+			subtitle: 'sass success',
+			message: 'sass update OK',
+			sound: false
+		}))
+		.pipe(browserSync.stream());
+	
+	done();
+}
+//Server
+function sync(done){
+	browserSync.init({
+		server:{
+			baseDir:'./project'
+		},
+		port:3000
+	});
+	done();
+}
+//ServerReload
+function borwserReload(done){
+	browserSync.reload()
+	done();
+}
+// watch
+function watch(){
+	gulp.watch('./project/assets/scss/main.scss',cssconfig);
+	gulp.watch('./project/*.html',borwserReload);
+	gulp.watch('./project/*.php',borwserReload);
+	gulp.watch('./project/*.js',borwserReload);
+
+}
+gulp.task('default', gulp.parallel(sync, watch));
+
